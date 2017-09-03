55bda26c5fc9b7378772329ccfb467767cee2453
SkulkingScavenger <SkulkingScavener@gmail.com>
2017-09-03 17:23:31 -0500

    clean repository

---
+++ gnoll_raider/src/Game.cpp
@@ -0,0 +1,13 @@
+class Game {
+	public:
+		/*GLboolean Keys[1024];
+		GLuint width, height;
+		
+		Game(GLuint width, GLuint height);
+		~Game();
+
+		void init();
+		void process_input(GLfloat delta_time);
+		void update(GLfloat delta_time);
+		void render();*/
+};
---
+++ gnoll_raider/src/Main.cpp
@@ -0,0 +1,14 @@
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, void** argv[]){
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
+	return 0;
+}
